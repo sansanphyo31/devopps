13b0361af35fbf66453802d28b61aa5e58cac74d
Phyoe Thuta <sansanphyo31@gmail.com>
2023-12-21 14:10:25 +0700

    dev2

---
+++ dev2.py
@@ -0,0 +1,23 @@
+name ="Phyoe Thuta"
+place ="Yangon"
+age = 23
+age2= 27
+print(f"My name is {name} , I live in {place} and {age} years old.")
+
+need = "AWS Certificate"
+need2 = "AWS Certificates"
+
+print("I need a %s" %need)
+print("I need %d %s" %(5,need2))
+
+print("I need a {}".format(need))
+print("I need %d {}".format(need2)%(5))
+
+a = 10 
+b = 3 
+print(a + b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
