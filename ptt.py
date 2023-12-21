a7a6f0de4c738c44bf1f90f1ada2c905f7a07d29
Phyoe Thuta <sansanphyo31@gmail.com>
2023-12-21 13:56:57 +0700

    final

--- ptt.py
+++ ptt.py
@@ -3,3 +3,10 @@ place ="Yangon"
 age = 23
 age2= 27
 print(f"My name is {name} , I live in {place} and {age} years old.")
+
+
+program = 'ccna cisco certified network associate'
+program2 = ' AWS'
+length = len(program)
+result = program.upper()
+print(result)
